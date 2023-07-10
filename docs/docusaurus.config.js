49854139c572ea463b98eb7403d5b4c5a4d7d6ac
Håkon <haakonu97@gmail.com>
2023-07-10 21:52:11 +0200

    fix: social card

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/typesafe-custom-event-social-card",
+      image: "img/typesafe-custom-event-social-card.png",
       footer: {
         style: "dark",
         links: [
